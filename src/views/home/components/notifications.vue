bda2f1903ae67687c4637adf9e05ed898e4f2d65
Fan Yikun <LuMingJun71543@outlook.com>
2023-04-26 16:40:44 +0100

    refactor

--- src/views/home/components/notifications.vue
+++ src/views/home/components/notifications.vue
@@ -1,42 +1,44 @@
 <template>
-  <el-row>
-    <el-col :span = "12">
-      <el-row v-for="stockData in urgent">
-        <el-col >
-          <el-card style="background-color: rgb(250,242,201)">
-            <p>warning:</p>
-            <p>product {{stockData.productName}} have batches of commodities near bbd</p>
-            <el-button @click="handleJump(stockData.productId)">
-              check details
-            </el-button>
-          </el-card>
-        </el-col>
-      </el-row>
-    </el-col>
-    <el-col :span = "12">
-      <el-row v-for="stockData in lowStock">
-        <el-col >
-          <el-card style="background-color: rgb(250,201,210)">
-            <p>warning:</p>
-            <p>product {{stockData.productName}} get only {{stockData.productStock}} left, which should at least have {{stockData.productLowStock}}</p>
-            <el-button @click="handleJump(stockData.productId)">
-              check details
-            </el-button>
-          </el-card>
-        </el-col>
-      </el-row>
-    </el-col>
-  </el-row>
+  <div style="margin: 10px">
+    <el-row>
+      <el-col :span = "12">
+        <el-row v-for="stockData in urgent">
+          <el-col>
+            <el-card style="background-color: rgb(250,242,201);margin: 10px">
+              <p>warning:</p>
+              <p>the commodity {{stockData.productName}} have batches of commodities are near bbd</p>
+              <el-button @click="handleJump(stockData.productId)">
+                check details
+              </el-button>
+            </el-card>
+          </el-col>
+        </el-row>
+      </el-col>
+      <el-col :span = "12">
+        <el-row v-for="stockData in lowStock">
+          <el-col >
+            <el-card style="background-color: rgb(250,201,210);margin: 10px">
+              <p>warning:</p>
+              <p>the commodity {{stockData.productName}} get only {{stockData.productStock}} left, which should at least have {{stockData.productLowStock}}</p>
+              <el-button @click="handleJump(stockData.productId)">
+                check details
+              </el-button>
+            </el-card>
+          </el-col>
+        </el-row>
+      </el-col>
+    </el-row>
+  </div>
 
 </template>
-<!--这里就是一个很好的例子，我可以说，我没有简单的读数据，我还要计算一下，
-因为他是每周固定时间发货，所以在发货日以前，有些货物是已经被预定了的，不应算在库存内的，
-这就需要我找出未处理订单，统计订单上的各种货物，再在库存中的货物中减去这-->
+
 <script>
 import axios from 'axios'
+import Commodities from '@/views/pms/abstractCommodities.vue'
 
 export default {
   name: 'notifications',
+  components: { Commodities },
   data(){
     return{
       lowStock:[
